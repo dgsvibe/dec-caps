9cf6d1270363ecf1f90c8b505f23e88d0758532c
Douglas Viana <42153643+dgsvibe@users.noreply.github.com>
2021-04-13 19:54:41 -0300

    Update caps.py

--- caps.py
+++ caps.py
@@ -3,18 +3,16 @@
 from itertools import product
 
 def series(C1,C2):
+    if C1 == 0 and C2 == 0:
+        return 0
     if C1 == 0:
         return C2
-    elif C2 == 0:
+    if C2 == 0:
         return C1
-    else:
-        return (((C1*10**(-6))*(C2*10**(-6)))/((C1*10**(-6))+(C2*10**(-6))))
+    return ((C1*C2)/(C1+C2))
 
 def parallel(C1,C2):
-    return ((C1*10**(-6))+(C2*10**(-6)))
-
-def show_result(result):
-    print(str(result*10**(6)))
+    return (C1+C2)
 
 def module(n):
     if n < 0:
@@ -22,18 +20,19 @@ def module(n):
     if n >= 0:
         return n
 
-caps = [0, 0.1, 0.22, 0.33, 0.47, 0.56, 0.68, 1, 2.2, 3.3, 4.7, 10] #Valores comerciais dos resistores
+caps = [0, 0.1, 0.22, 0.33, 0.47, 0.56, 0.68, 1, 2.2, 3.3, 4.7, 10] #Valores comerciais dos resistores em uF
 capsPermList = []
 
 genComb = product(caps, repeat=4) # aqui e onde tens de especificar o numero de chars que cada combinacao tenha
 for subset in genComb:
     capsPermList.append(subset)
 
-capacitanciaDesejada = 3.9*10**(-6)
-diferencaMaximaDesejada = .05*10**(-6)
+capacitanciaParaDecompor = 3.9
+diferencaMaximaDesejada = .01
 
 
-for conj in capsPermList:    
-        r = module(capacitanciaDesejada - series(parallel(conj[0],conj[1]),parallel(conj[2],conj[3])))
-        if r <= diferencaMaximaDesejada:
-            print(f'{r} (R1,R2,R3,R4) = ({conj[0]},{conj[1]},{conj[2]},{conj[3]})') #(R1 || R2) - (R3 || R4)
+for conj in capsPermList:
+    s = series(parallel(conj[0],conj[1]),parallel(conj[2],conj[3]))
+    r = module(capacitanciaParaDecompor - s)
+    if r <= diferencaMaximaDesejada:
+        print(f'Diferença = {r} ({conj[0]} || {conj[1]}) <-> ({conj[2]} || {conj[3]}) uF') #(R1 || R2) - (R3 || R4)
